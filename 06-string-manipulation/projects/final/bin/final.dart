f899ca7a4607102f92031c678c56a37427625377
suragch <suragch@suragch.dev>
2022-04-11 11:43:42 +0800

    Chapter 7 updates

--- 06-string-manipulation/projects/final/bin/final.dart
+++ 06-string-manipulation/projects/final/bin/final.dart
@@ -2,29 +2,40 @@
 // For full license & permission details, see LICENSE.
 
 void main() {
-  var userInput = 'sPoNgEbOb@eXaMpLe.cOm';
-  final lowercase = userInput.toUpperCase();
-  print(lowercase);
-
-  userInput = ' 221B Baker St.   ';
-  final trimmed = userInput.trim();
-  print("'$trimmed'");
-
-  var time = Duration(hours: 1, minutes: 32, seconds: 57);
-  var hours = time.inHours;
-  var minutes = time.inMinutes % 60;
-  var seconds = time.inSeconds % 60;
-  var timeString = '$hours:$minutes:$seconds';
-  print(timeString); // 1:32:57
-
+  changingCase();
+  trimming();
   padding();
   splittingAndJoining();
   findAndReplace();
   stringBuffer();
-  stringValidation();
+  // stringValidation();
+  // regex();
+  // validatePassword();
+  // extractingText();
+  // substringMultipleMatches();
+  // regexGroups();
+}
+
+void changingCase() {
+  const userInput = 'sPoNgEbOb@eXaMpLe.cOm';
+  final lowercase = userInput.toLowerCase();
+  print(lowercase);
+}
+
+void trimming() {
+  const userInput = ' 221B Baker St.   ';
+  final trimmed = userInput.trim();
+  print(trimmed);
 }
 
 void padding() {
+  // final time = Duration(hours: 1, minutes: 32, seconds: 57);
+  // final hours = time.inHours;
+  // final minutes = time.inMinutes % 60;
+  // final seconds = time.inSeconds % 60;
+  // final timeString = '$hours:$minutes:$seconds';
+  // print(timeString);
+
   final time = Duration(hours: 1, minutes: 2, seconds: 3);
   final hours = time.inHours;
   final minutes = '${time.inMinutes % 60}'.padLeft(2, '0');
@@ -34,7 +45,7 @@ void padding() {
 }
 
 void splittingAndJoining() {
-  const csvFileLine = 'Bob,Smith,37,Paris,France';
+  const csvFileLine = 'Martin,Emma,12,Paris,France';
   final fields = csvFileLine.split(',');
   print(fields);
 
@@ -49,6 +60,18 @@ void findAndReplace() {
 }
 
 void stringBuffer() {
+  // var message = 'Hello' + ' my name is ';
+  // const name = 'Ray';
+  // message += name;
+  // 'Hello my name is Ray'
+
+  final message = StringBuffer();
+  message.write('Hello');
+  message.write(' my name is ');
+  message.write('Ray');
+  message.toString();
+  // 'Hello my name is Ray'
+
   for (int i = 2; i <= 1024; i *= 2) {
     print(i);
   }
@@ -69,4 +92,131 @@ void stringValidation() {
   print(text.contains('Flutter'));
 
   const password = 'password123';
+}
+
+void regex() {
+  final regex = RegExp('cat');
+  print(regex.hasMatch('cat'));
+  print(regex.hasMatch('dog'));
+  print(regex.hasMatch('cats'));
+  print('cat'.contains(regex));
+  print('dog'.contains(regex));
+  print('cats'.contains(regex));
+
+  final matchSingle = RegExp('c.t');
+  print(matchSingle.hasMatch('cat'));
+  print(matchSingle.hasMatch('cot'));
+  print(matchSingle.hasMatch('cut'));
+  print(matchSingle.hasMatch('ct'));
+
+  final oneOrMore = RegExp('wo+w');
+  print(oneOrMore.hasMatch('ww'));
+  print(oneOrMore.hasMatch('wow'));
+  print(oneOrMore.hasMatch('wooow'));
+  print(oneOrMore.hasMatch('wooooooow'));
+
+  final zeroOrMore = RegExp('wo*w');
+  print(zeroOrMore.hasMatch('ww'));
+  print(zeroOrMore.hasMatch('wow'));
+  print(zeroOrMore.hasMatch('wooow'));
+  print(zeroOrMore.hasMatch('wooooooow'));
+
+  final set = RegExp('b[oa]t');
+  print(set.hasMatch('bat')); // true
+  print(set.hasMatch('bot')); // true
+  print(set.hasMatch('but')); // false
+  print(set.hasMatch('boat')); // false
+  print(set.hasMatch('bt')); // false
+
+  final letters = RegExp('[a-zA-Z]');
+  print(letters.hasMatch('a')); // true
+  print(letters.hasMatch('H')); // true
+  print(letters.hasMatch('3z')); // true
+  print(letters.hasMatch('2')); // false
+
+  final escaped = RegExp(r'c\.t');
+  print(escaped.hasMatch('c.t'));
+  print(escaped.hasMatch('cat'));
+}
+
+void validatePassword() {
+  const password = 'Password1234';
+
+  final lowercase = RegExp(r'[a-z]');
+  final uppercase = RegExp(r'[A-Z]');
+  final number = RegExp(r'[0-9]');
+
+  if (!password.contains(lowercase)) {
+    print('Your password must have a lowercase letter!');
+  } else if (!password.contains(uppercase)) {
+    print('Your password must have an uppercase letter!');
+  } else if (!password.contains(number)) {
+    print('Your password must have a number!');
+  } else {
+    print('Your password is OK.');
+  }
+
+  if (password.length < 12) {
+    print('Your password must be at least 12 characters long!');
+  }
+
+  final goodLength = RegExp(r'.{12,}');
+  if (!password.contains(goodLength)) {
+    print('Your password must be at least 12 characters long!');
+  }
+}
+
+void extractingText() {
+  const htmlText = '''
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Dart Tutorial</h1>
+<p>Dart is the best language.</p>
+</body>
+</html> 
+''';
+
+  var heading = htmlText.substring(34, 47);
+  print(heading);
+
+  final start = htmlText.indexOf('<h1>') + '<h1>'.length;
+  final end = htmlText.indexOf('</h1>');
+  heading = htmlText.substring(start, end);
+  print(heading);
+}
+
+void substringMultipleMatches() {
+  const text = '''
+<h1>Dart Tutorial</h1>
+<h1>Flutter Tutorial</h1>
+<h1>Other Tutorials</h1>
+''';
+
+  var position = 0;
+  while (true) {
+    var start = text.indexOf('<h1>', position) + '<h1>'.length;
+    var end = text.indexOf('</h1>', position);
+    if (start == -1 || end == -1) {
+      break;
+    }
+    final heading = text.substring(start, end);
+    print(heading);
+    position = end + '</h1>'.length;
+  }
+}
+
+void regexGroups() {
+  const text = '''
+<h1>Dart Tutorial</h1>
+<h1>Flutter Tutorial</h1>
+<h1>Other Tutorials</h1>
+''';
+
+  final headings = RegExp(r'<h1>(.+)</h1>');
+  final matches = headings.allMatches(text);
+
+  for (final match in matches) {
+    print(match.group(1));
+  }
 }
