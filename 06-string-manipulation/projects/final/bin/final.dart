dd85558a5582dba4b715ff8989e77de8f6bcac36
suragch <suragch@suragch.dev>
2022-04-11 16:49:34 +0800

    final finished but need challenges

--- 06-string-manipulation/projects/final/bin/final.dart
+++ 06-string-manipulation/projects/final/bin/final.dart
@@ -2,18 +2,30 @@
 // For full license & permission details, see LICENSE.
 
 void main() {
+  /// Basic String Manipulation
   changingCase();
   trimming();
   padding();
   splittingAndJoining();
   findAndReplace();
+
+  /// Building Strings
   stringBuffer();
-  // stringValidation();
-  // regex();
-  // validatePassword();
-  // extractingText();
-  // substringMultipleMatches();
-  // regexGroups();
+
+  /// String Validation
+  stringValidation();
+  matchingLiteralCharacters();
+  matchingAnySingleCharacter();
+  matchingMultipleCharacters();
+  matchingSetsOfCharacters();
+  escapingSpecialCharacters();
+  matchingBeginningEndLine();
+  validatePassword();
+
+  /// Extracting Text
+  extractingText();
+  substringMultipleMatches();
+  regexGroups();
 }
 
 void changingCase() {
@@ -85,16 +97,18 @@ void stringBuffer() {
 }
 
 void stringValidation() {
+  print('String Validation:');
+
   const text = 'I love Dart';
   print(text.startsWith('I'));
   print(text.endsWith('Dart'));
   print(text.contains('love'));
   print(text.contains('Flutter'));
-
-  const password = 'password123';
 }
 
-void regex() {
+void matchingLiteralCharacters() {
+  print('Matching Literal Characters:');
+
   final regex = RegExp('cat');
   print(regex.hasMatch('cat'));
   print(regex.hasMatch('dog'));
@@ -102,12 +116,26 @@ void regex() {
   print('cat'.contains(regex));
   print('dog'.contains(regex));
   print('cats'.contains(regex));
+}
+
+void matchingAnySingleCharacter() {
+  print('Matching Any Single Character');
 
   final matchSingle = RegExp('c.t');
   print(matchSingle.hasMatch('cat'));
   print(matchSingle.hasMatch('cot'));
   print(matchSingle.hasMatch('cut'));
   print(matchSingle.hasMatch('ct'));
+
+  final optionalSingle = RegExp('c?t');
+  print(optionalSingle.hasMatch('cat'));
+  print(optionalSingle.hasMatch('cot'));
+  print(optionalSingle.hasMatch('cut'));
+  print(optionalSingle.hasMatch('ct'));
+}
+
+void matchingMultipleCharacters() {
+  print('Matching Multiple Characters');
 
   final oneOrMore = RegExp('wo+w');
   print(oneOrMore.hasMatch('ww'));
@@ -121,22 +149,55 @@ void regex() {
   print(zeroOrMore.hasMatch('wooow'));
   print(zeroOrMore.hasMatch('wooooooow'));
 
+  final anyOneOrMore = RegExp('w.+w');
+  print(anyOneOrMore.hasMatch('ww'));
+  print(anyOneOrMore.hasMatch('wow'));
+  print(anyOneOrMore.hasMatch('w123w'));
+  print(anyOneOrMore.hasMatch('wABCDEFGw'));
+}
+
+void matchingSetsOfCharacters() {
+  print('Matching Sets Of Characters:');
+
   final set = RegExp('b[oa]t');
-  print(set.hasMatch('bat')); // true
-  print(set.hasMatch('bot')); // true
-  print(set.hasMatch('but')); // false
-  print(set.hasMatch('boat')); // false
-  print(set.hasMatch('bt')); // false
+  print(set.hasMatch('bat'));
+  print(set.hasMatch('bot'));
+  print(set.hasMatch('but'));
+  print(set.hasMatch('boat'));
+  print(set.hasMatch('bt'));
 
   final letters = RegExp('[a-zA-Z]');
-  print(letters.hasMatch('a')); // true
-  print(letters.hasMatch('H')); // true
-  print(letters.hasMatch('3z')); // true
-  print(letters.hasMatch('2')); // false
+  print(letters.hasMatch('a'));
+  print(letters.hasMatch('H'));
+  print(letters.hasMatch('3z'));
+  print(letters.hasMatch('2'));
+
+  final excluded = RegExp('b[^ao]t');
+  print(excluded.hasMatch('bat')); // false
+  print(excluded.hasMatch('bot')); // false
+  print(excluded.hasMatch('but')); // true
+  print(excluded.hasMatch('boat')); // false
+  print(excluded.hasMatch('bt')); // false
+}
+
+void escapingSpecialCharacters() {
+  print('Escaping Special Characters:');
 
   final escaped = RegExp(r'c\.t');
   print(escaped.hasMatch('c.t'));
   print(escaped.hasMatch('cat'));
+}
+
+void matchingBeginningEndLine() {
+  print('Matching Beginning and End of Line:');
+
+  final numbers = RegExp(r'[0-9]');
+  print(numbers.hasMatch('5552021'));
+  print(numbers.hasMatch('abcefg2'));
+
+  final onlyNumbers = RegExp(r'^[0-9]+$');
+  print(onlyNumbers.hasMatch('5552021'));
+  print(onlyNumbers.hasMatch('abcefg2'));
 }
 
 void validatePassword() {
