60e22195362ac89fbe0ec7fbbbc8c4434a7cc895
suragch <suragch@suragch.dev>
2022-04-16 16:44:41 +0800

    Classes Chapter update

--- 07-classes/projects/final/bin/final.dart
+++ 07-classes/projects/final/bin/final.dart
@@ -1,74 +1,35 @@
-// Copyright (c) 2021 Razeware LLC
+// Copyright (c) 2022 Razeware LLC
 // For full license & permission details, see LICENSE.
 
 import 'user.dart';
 
 void main() {
-  dartClasses();
-  constructors();
-  dartObjects();
-  staticMembers();
-}
-
-void dartClasses() {
-  final user = User();
-  // final user = new User();
-  // user.name = 'Ray';
-  // user.id = 42;
-  print(user);
-  print(user.toJson());
-}
-
-void constructors() {
-  // final user = User(42, 'Ray');
-  // final user = User(id: 42, name: 'Ray');
-  // final user = User(_id: 42, _name: 'Ray');
-  // final anonymousUser = User.anonymous();
   final vicki = User(id: 24, name: 'Vicki');
-  // final vicki = User(_id: 24, _name: 'Vicki');
-  // vicki.name = 'Nefarious Hacker';
   // vicki._name = 'Nefarious Hacker';
-  print(vicki);
-  // final vicki = User(id: 24, name: 'Vicki');
-  // vicki._name = 'Nefarious Hacker';
-  // final vicki = User(id: 24, name: 'Vicki');
-  // vicki._name = 'Nefarious Hacker';
-  // final jb = User(id: -1, name: 'JB Lorenzo');
-  final jb = User(id: 100, name: 'JB Lorenzo');
-  print(jb);
   const user = User(id: 42, name: 'Ray');
-  print(user);
   const anonymousUser = User.anonymous();
+  print(vicki);
+  print(user);
   print(anonymousUser);
-  final map = {'id': 10, 'name': 'Manda'};
-  final manda = User.fromJson(map);
-  print(manda);
-}
 
-void dartObjects() {
   final myObject = MyClass();
   final anotherObject = myObject;
-  print(myObject.myProperty); // 1
+  print(myObject.myProperty);
   anotherObject.myProperty = 2;
-  print(myObject.myProperty); // 2
+  print(myObject.myProperty);
+
   const ray = User(id: 42, name: 'Ray');
   print(ray.id);
   print(ray.name);
-  // final email = Email();
-  // email.value = 'ray@example.com';
+
   final email = Email('ray@example.com');
   final emailString = email.value;
   print(emailString);
-}
 
-void staticMembers() {
   final value = SomeClass.myProperty;
   SomeClass.myMethod();
-  final mySingleton = MySingleton.instance;
-  // final user = User.fromJson('{"id":42,"name":"Ray"}');
-  final map = {'id': 10, 'name': 'Manda'};
-  final manda = User.fromJson(map);
-  print(manda);
+
+  final mySingleton = MySingleton();
 }
 
 class Address {
@@ -76,16 +37,13 @@ class Address {
   var value = '';
 }
 
-class Email {
-  Email(this.value);
-  final value;
+class MyClass {
+  var myProperty = 1;
 }
 
-class MyClass {
-  int myProperty = 0;
-  void myMethod() {
-    print('Hello, Dart!');
-  }
+class Email {
+  Email(this.value);
+  final String value;
 }
 
 class SomeClass {
@@ -97,5 +55,6 @@ class SomeClass {
 
 class MySingleton {
   MySingleton._();
-  static final MySingleton instance = MySingleton._();
+  static final MySingleton _instance = MySingleton._();
+  factory MySingleton() => _instance;
 }
--- 07-classes/projects/final/bin/user.dart
+++ 07-classes/projects/final/bin/user.dart
@@ -1,29 +1,13 @@
-// Copyright (c) 2021 Razeware LLC
+// Copyright (c) 2022 Razeware LLC
 // For full license & permission details, see LICENSE.
 
 class User {
   const User({
     this.id = _anonymousId,
     this.name = _anonymousName,
-  }) : assert(id >= 0);
+  });
 
   const User.anonymous() : this();
-
-  // User.fromJson(Map<String, Object> json)
-  //     : id = json['id'] as int,
-  //       name = json['name'] as String;
-
-  // factory User.fromJson(Map<String, Object> json) {
-  //   final userId = json['id'] as int;
-  //   final userName = json['name'] as String;
-  //   return User(id: userId, name: userName);
-  // }
-
-  static User fromJson(Map<String, Object> json) {
-    final userId = json['id'] as int;
-    final userName = json['name'] as String;
-    return User(id: userId, name: userName);
-  }
 
   final String name;
   final int id;
@@ -33,6 +17,12 @@ class User {
 
   String toJson() {
     return '{"id":$id,"name":"$name"}';
+  }
+
+  static User fromJson(Map<String, Object> json) {
+    final userId = json['id'] as int;
+    final userName = json['name'] as String;
+    return User(id: userId, name: userName);
   }
 
   @override
