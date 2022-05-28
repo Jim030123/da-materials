e307b58b81cf6733a7c15b61210de4ed49fe45d9
suragch <suragch@suragch.dev>
2022-05-28 19:35:01 +0800

    work on final and challenge

--- 13-anonymous-citizens/projects/challenge/bin/challenge.dart
+++ 13-anonymous-citizens/projects/challenge/bin/challenge.dart
@@ -1,4 +1,29 @@
 // Copyright (c) 2022 Razeware LLC
 // For full license & permission details, see LICENSE.
 
-void main() {}
+void main() {
+  higherOrderFunctionsExercise();
+}
+
+/// Exercise: Higher-Order Functions
+///
+/// Given the following exam scores:
+///
+/// ```dart
+/// final scores = [89, 77, 46, 93, 82, 67, 32, 88];
+/// ```
+///
+/// 1. Use `sort` to find the highest and lowest grades.
+/// 2. Use `where` to find all the B grades, that is, all the scores
+///    between `80` and `90`.
+void higherOrderFunctionsExercise() {
+  final scores = [89, 77, 46, 93, 82, 67, 32, 88];
+  scores.sort();
+  final highest = scores.first;
+  final lowest = scores.last;
+  print(highest);
+  print(lowest);
+
+  final bGrades = scores.where((score) => score >= 80 && score < 90);
+  print(bGrades);
+}
--- 13-anonymous-citizens/projects/final/bin/final.dart
+++ 13-anonymous-citizens/projects/final/bin/final.dart
@@ -1,4 +1,219 @@
 // Copyright (c) 2022 Razeware LLC
 // For full license & permission details, see LICENSE.
 
-void main() {}
+void main() {
+  assigningFunctionsToVariables();
+  functionsAsParameters();
+  functionsAsReturnValues();
+  forEachLoopsOnList();
+  forEachLoopsOnMap();
+  mapping();
+  filtering();
+  reducing();
+  folding();
+  sorting();
+  combiningHigherOrderMethods();
+  voidCallback();
+  valueSetterCallback();
+  valueGetterCallback();
+}
+
+void assigningFunctionsToVariables() {
+  int number = 4;
+  String greeting = 'hello';
+  bool isHungry = true;
+
+  Function multiply = (int a, int b) {
+    return a * b;
+  };
+}
+
+void functionsAsParameters() {
+  void namedFunction(Function anonymousFunction) {
+    // function body
+  }
+}
+
+void functionsAsReturnValues() {
+  Function namedFunction() {
+    return () => print('hello');
+  }
+}
+
+void forEachLoopsOnList() {
+  const numbers = [1, 2, 3];
+
+  numbers.forEach((int number) {
+    print(3 * number);
+  });
+
+  numbers.forEach((number) {
+    print(3 * number);
+  });
+
+  numbers.forEach((number) => print(3 * number));
+
+  for (final number in numbers) {
+    print(3 * number);
+  }
+
+  final triple = (int x) => print(3 * x);
+  numbers.forEach(triple);
+}
+
+void forEachLoopsOnMap() {
+  final flowerColor = {
+    'roses': 'red',
+    'violets': 'blue',
+  };
+
+  flowerColor.forEach((flower, color) {
+    print('$flower are $color');
+  });
+
+  print('i \u2764 Dart');
+  print('and so do you');
+}
+
+void mapping() {
+  const numbers = [2, 4, 6, 8, 10, 12];
+
+  final looped = <int>[];
+  for (final x in numbers) {
+    looped.add(x * x);
+  }
+  print(looped);
+
+  final mapped = numbers.map((x) => x * x);
+  print(mapped);
+  print(mapped.toList());
+}
+
+void filtering() {
+  final myList = [1, 2, 3, 4, 5, 6];
+  final odds = myList.where((element) => element.isOdd);
+  print(odds);
+}
+
+void reducing() {
+  const evens = [2, 4, 6, 8, 10, 12];
+  final total = evens.reduce((sum, element) => sum + element);
+  print(total);
+
+  // final emptyList = <int>[];
+  // final result = emptyList.reduce((sum, x) => sum + x);
+}
+
+void folding() {
+  const evens = [2, 4, 6, 8, 10, 12];
+  final total = evens.fold<int>(
+    0,
+    (int sum, element) => sum + element,
+  );
+  print(total);
+}
+
+void sorting() {
+  final desserts = ['cookies', 'pie', 'donuts', 'brownies'];
+
+  desserts.sort();
+  print(desserts);
+
+  desserts.sort((d1, d2) => d1.length.compareTo(d2.length));
+  print(desserts);
+}
+
+void combiningHigherOrderMethods() {
+  declarative();
+  imperative();
+}
+
+void declarative() {
+  const desserts = ['cake', 'pie', 'donuts', 'brownies'];
+  final bigTallDesserts = desserts
+      .where((dessert) => dessert.length > 5)
+      .map((dessert) => dessert.toUpperCase())
+      .toList();
+  print(bigTallDesserts);
+}
+
+void imperative() {
+  const desserts = ['cake', 'pie', 'donuts', 'brownies'];
+  final bigTallDesserts = <String>[];
+  for (final item in desserts) {
+    if (item.length > 5) {
+      final upperCase = item.toUpperCase();
+      bigTallDesserts.add(upperCase);
+    }
+  }
+  print(bigTallDesserts);
+}
+
+class Button {
+  Button({
+    required this.title,
+    required this.onPressed,
+  });
+
+  final String title;
+  final void Function() onPressed;
+}
+
+void voidCallback() {
+  final myButton = Button(
+    title: 'Click me!',
+    onPressed: () {
+      print('Clicked');
+    },
+  );
+  myButton.onPressed();
+  myButton.onPressed.call();
+
+  // final anotherButton = Button(
+  //   title: 'Click me, too!',
+  //   onPressed: (int apple) {
+  //     print('Clicked');
+  //     return 42;
+  //   },
+  // );
+}
+
+class MyWidget {
+  MyWidget({
+    required this.onTouch,
+  });
+
+  final void Function(double xPosition) onTouch;
+}
+
+void valueSetterCallback() {
+  final myWidget = MyWidget(
+    onTouch: (x) => print(x),
+  );
+  myWidget.onTouch(3.14);
+}
+
+class AnotherWidget {
+  AnotherWidget({
+    this.timeStamp,
+  });
+
+  final String Function()? timeStamp;
+}
+
+void valueGetterCallback() {
+  final myWidget = AnotherWidget(
+    timeStamp: () => DateTime.now().toIso8601String(),
+  );
+
+  final timeStamp = myWidget.timeStamp?.call();
+  print(timeStamp);
+}
+
+class StateManager {
+  int _counter = 0;
+
+  void handleButtonClick() {
+    _counter++;
+  }
+}
