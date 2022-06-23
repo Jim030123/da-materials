525feac4f6c79a1fae1a8c69fb7d0937a67f4e11
suragch <suragch@suragch.dev>
2022-06-23 15:54:35 -0700

    Static members: final draft

--- 10-static-members/projects/final/bin/final.dart
+++ 10-static-members/projects/final/bin/final.dart
@@ -1,205 +1,19 @@
 // Copyright (c) 2022 Razeware LLC
 // For full license & permission details, see LICENSE.
 
-import 'user.dart';
+import 'math.dart' as math;
 
 void main() {
-  // Dart Classes
-  definingAClass();
-  creatingAnObjectFromAClass();
-  addingAJsonSerializationMethod();
-
-  // Constructors
-  defaultConstructor();
-  longFormConstructor();
-  shortFormConstructor();
-  namedConstructors();
-  forwardingConstructors();
-  optionalAndNamedParameters();
-  initializerLists();
-  makingClassesImmutable();
-
-  // Dart Objects
-  dartObjects();
-  getters();
-  setters();
-
-  // Static Members
-  staticMembers();
-  staticVariables();
+  introduction();
+  constants();
+  singletons();
   staticMethods();
+  creatingNewObjects();
 }
 
-void definingAClass() {
-  // class User {
-  //   int id = 0;
-  //   String name = '';
-  // }
-}
-
-void creatingAnObjectFromAClass() {
-  // class User {
-  //   int id = 0;
-  //   String name = '';
-  // }
-  final user = User();
-  // user.name = 'Ray';
-  // user.id = 42;
-
-  print(user);
-}
-
-void addingAJsonSerializationMethod() {
-  final user = User();
-  print(user.toJson());
-}
-
-void cascadeNotation() {
-  // final user = User()
-  // ..name = 'Ray'
-  // ..id = 42;
-}
-
-void defaultConstructor() {
-  // class Address {
-  //   var value = '';
-  // }
-}
-
-void longFormConstructor() {
-  // class User {
-  //   User(int id, String name) {
-  //     this.id = id;
-  //     this.name = name;
-  //   }
-
-  //   int id = 0;
-  //   String name = '';
-
-  //   // ...
-  // }
-
-  // final user = User(42, 'Ray');
-  // print(user);
-}
-
-void shortFormConstructor() {
-  // class User {
-  //   User(this.id, this.name);
-  //   int id = 0;
-  //   String name = '';
-  //   // ...
-  // }
-}
-
-void namedConstructors() {
-  // User.anonymous() {
-  //   id = 0;
-  //   name = 'anonymous';
-  // }
-  const anonymousUser = User.anonymous();
-  print(anonymousUser);
-}
-
-void forwardingConstructors() {
-  // User.anonymous() : this(0, 'anonymous');
-  final anonymousUser = User.anonymous();
-  print(anonymousUser);
-}
-
-void optionalAndNamedParameters() {
-  // MyClass([this.myProperty]);
-  // MyClass({this.myProperty});
-  // MyClass({required this.myProperty});
-
-  // final user = User(42, 'Ray');
-  // User({this.id = 0, this.name = 'anonymous'});
-  final user = User(id: 42, name: 'Ray');
-  print(user);
-}
-
-void initializerLists() {
-// User({int id = 0, String name = 'anonymous'})
-//     : _id = id,
-//       _name = name;
-
-//   User({int id = 0, String name = 'anonymous'})
-//     : _id = id,
-//       _name = name {
-//   print('User name is $_name');
-// }
-
-  final vicki = User(id: 24, name: 'Vicki');
-  // vicki._name = 'Nefarious Hacker';
-
-  print(vicki);
-}
-
-void makingClassesImmutable() {
-  const user = User(id: 42, name: 'Ray');
-  const anonymousUser = User.anonymous();
-  print(user);
-  print(anonymousUser);
-}
-
-void dartObjects() {
-  final myObject = MyClass();
-  final anotherObject = myObject;
-  print(myObject.myProperty);
-  anotherObject.myProperty = 2;
-  print(myObject.myProperty);
-}
-
-void getters() {
-  const ray = User(id: 42, name: 'Ray');
-  print(ray.id);
-  print(ray.name);
-}
-
-void setters() {
-  // final email = Email();
-  // email.value = 'ray@example.com';
-  // final emailString = email.value;
-
-  final email = Email('ray@example.com');
-  final emailString = email.value;
-  print(emailString);
-}
-
-void staticMembers() {
+void introduction() {
   final value = SomeClass.myProperty;
   SomeClass.myMethod();
-}
-
-void staticVariables() {
-  final mySingleton = MySingleton();
-}
-
-void staticMethods() {
-  final map = {'id': 10, 'name': 'Sandra'};
-  final sandra = User.fromJson(map);
-  print(sandra);
-}
-
-class Address {
-  Address();
-  var value = '';
-}
-
-class MyClass {
-  var myProperty = 1;
-}
-
-// class Email {
-//   var _address = '';
-
-//   String get value => _address;
-//   set value(String address) => _address = address;
-// }
-
-class Email {
-  Email(this.value);
-  final String value;
 }
 
 class SomeClass {
@@ -209,8 +23,95 @@ class SomeClass {
   }
 }
 
+class TextStyle {
+  static const _defaultFontSize = 17.0;
+
+  TextStyle({this.fontSize = _defaultFontSize});
+  final double fontSize;
+}
+
+class Colors {
+  static const int red = 0xFFD13F13;
+  static const int purple = 0xFF8107D9;
+  static const int blue = 0xFF1432C9;
+}
+
+void constants() {
+  final backgroundColor = Colors.purple;
+  print(backgroundColor);
+}
+
 class MySingleton {
   MySingleton._();
-  static final MySingleton _instance = MySingleton._();
-  factory MySingleton() => _instance;
+  static final MySingleton instance = MySingleton._();
+}
+
+// class MySingleton {
+//   MySingleton._();
+//   static final MySingleton _instance = MySingleton._();
+//   factory MySingleton() => _instance;
+// }
+
+void singletons() {
+  final mySingleton = MySingleton.instance;
+  // final mySingleton = MySingleton();
+}
+
+class Math {
+  static num max(num a, num b) {
+    return (a > b) ? a : b;
+  }
+
+  static num min(num a, num b) {
+    return (a < b) ? a : b;
+  }
+}
+
+void staticMethods() {
+  print(Math.max(2, 3));
+  print(Math.min(2, 3));
+
+  // print(max(2, 3));
+  // print(min(2, 3));
+
+  print(math.max(2, 3));
+  print(math.min(2, 3));
+}
+
+class User {
+  const User({int id = 0, String name = 'anonymous'})
+      : _id = id,
+        _name = name;
+
+  const User.anonymous() : this();
+
+  // factory User.fromJson(Map<String, Object> json) {
+  //   final userId = json['id'] as int;
+  //   final userName = json['name'] as String;
+  //   return User(id: userId, name: userName);
+  // }
+
+  static User fromJson(Map<String, Object> json) {
+    final userId = json['id'] as int;
+    final userName = json['name'] as String;
+    return User(id: userId, name: userName);
+  }
+
+  final String _name;
+  final int _id;
+
+  String toJson() {
+    return '{"id":$_id,"name":"$_name"}';
+  }
+
+  @override
+  String toString() {
+    return 'User(id: $_id, name: $_name)';
+  }
+}
+
+void creatingNewObjects() {
+  final map = {'id': 10, 'name': 'Sandra'};
+  final sandra = User.fromJson(map);
+  print(sandra);
 }
